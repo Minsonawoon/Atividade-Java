3c5fb416017d2590132e83c47df6e0563635135b
Isabela Clemente da Silva <bela.mi.jas@gmail.com>
2024-12-05 20:32:44 -0300

    adição dos exercícios de lógica

--- src/main/java/exercicioslogica/Ex56.java
+++ src/main/java/exercicioslogica/Ex56.java
@@ -1,4 +1,25 @@
 package exercicioslogica;
+import java.util.Scanner;
 
 public class Ex56 {
+    public static void main(String[] args) {
+//        56) Crie um programa que leia um número inteiro positivo e exiba todos os divisores desse número usando o loop "do-while".
+
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite um número inteiro positivo:");
+        int numero = entrada.nextInt();
+
+        int i = 1;
+
+        System.out.println("Os divisores de " + numero + " são:");
+
+        do {
+            if (numero % i == 0) {
+                System.out.println(i);
+            }
+            i++;
+        } while (i <= numero);
+    }
 }
+
